8b6de95958435c898304b501ff40335b98c3c8fd
torsten.dix <torsten.dix@cbr.de>
2021-04-07 16:45:26 +0200

    Creating ForNext-Stack-Class

--- ForNextDefinition.cpp
+++ ForNextDefinition.cpp
@@ -12,19 +12,26 @@ private:
     Value step;
 public:
 ForNextDefinition::ForNextDefinition(VarDefinition varDef, std::vector<int> varIndex, bool isAscending, Value counter, Value target, Value step) {
-    this->varDef = varDef;
-    this->varIndex = varIndex;
+    this->varDef      = varDef;
+    this->varIndex    = varIndex;
     this->isAscending = isAscending;
-    this->counter = counter;
-    this->target  = target;
-    this->step    = step;
+    this->counter     = counter;
+    this->target      = target;
+    this->step        = step;
 }
 
 bool ForNextDefinition::checkNextStep(VarDefinition varDef, std::vector<int> varIndex) {
-
+    if (varDef != this->varDef || varIndex.count() != this->varIndex.count()) {
+        throw ForNextDefinition(EXCEPTION_NEXT_WITHOUT_FOR);
+    }
+    return ForNextDefinition::checkNextStep();
 }
 
 bool ForNextDefinition::checkNextStep() {
-
+    // Check Stack
+    // count counter
+    // compare
+    // if false => remove from stack
+    // if true => jump to stack-adress
 }
 
